3d8954e7693b621eb07194e080232155279a3e6b
Jamal Neufeld <me@jamal.io>
2024-12-09 14:23:43 -0500

    [main] day 9

---
+++ src/bin/9/main.rs
@@ -0,0 +1,136 @@
+fn get_disk_map(puzzle: &Vec<usize>) -> Vec<i64> {
+    let mut disk_map: Vec<i64> = Vec::new();
+
+    for (id, chunk) in puzzle.chunks(2).enumerate() {
+        let id = id as i64;
+        match chunk {
+            [file_size, space_size] => {
+                disk_map.extend(vec![id; *file_size]);
+                disk_map.extend(vec![-1; *space_size])
+            }
+            [file_size] => disk_map.extend(vec![id; *file_size]),
+            _ => {
+                panic!("Error at index: {}", id);
+            }
+        }
+    }
+
+    disk_map
+}
+
+fn part1(puzzle: &str) -> i64 {
+    let puzzle: Vec<usize> = puzzle
+        .chars()
+        .map(|x| x.to_string().parse().unwrap())
+        .collect();
+
+    let disk_map = get_disk_map(&puzzle);
+
+    let mut checksum = 0;
+
+    let mut reverse_index = disk_map.len();
+
+    for forward_index in 0..disk_map.len() {
+        if forward_index >= reverse_index {
+            break;
+        };
+        let id = disk_map[forward_index];
+        if id == -1 {
+            reverse_index -= 1;
+            while disk_map[reverse_index] == -1 {
+                reverse_index -= 1;
+            }
+            let reverse_id = disk_map[reverse_index];
+            checksum += reverse_id * forward_index as i64;
+        } else {
+            checksum += id * forward_index as i64;
+        }
+    }
+
+    checksum
+}
+
+fn part2(puzzle: &str) -> i64 {
+    let puzzle: Vec<usize> = puzzle
+        .chars()
+        .map(|x| x.to_string().parse().unwrap())
+        .collect();
+
+    let mut disk_map: Vec<i64> = Vec::new();
+
+    let mut moved_ids = Vec::new();
+
+    for (id, chunk) in puzzle.chunks(2).enumerate() {
+        let mut reverse_index = if puzzle.len() % 2 == 0 {
+            puzzle.len() - 2
+        } else {
+            puzzle.len() - 1
+        };
+
+        match chunk {
+            [file_size, space_size] => {
+                let mut space_size = space_size.clone();
+
+                disk_map.extend(vec![
+                    if moved_ids.contains(&id) {
+                        -1
+                    } else {
+                        id as i64
+                    };
+                    *file_size
+                ]);
+
+                while reverse_index > id * 2 {
+                    let reverse_file_id = reverse_index / 2;
+                    let reverse_file_size = puzzle[reverse_index];
+
+                    if reverse_file_size <= space_size && !moved_ids.contains(&reverse_file_id) {
+                        disk_map.extend(vec![reverse_file_id as i64; reverse_file_size]);
+                        space_size -= reverse_file_size;
+                        moved_ids.push(reverse_file_id);
+                    }
+                    reverse_index -= 2;
+                }
+                disk_map.extend(vec![-1; space_size]);
+            }
+            [file_size] => disk_map.extend(vec![
+                if moved_ids.contains(&id) {
+                    -1
+                } else {
+                    id as i64
+                };
+                *file_size
+            ]),
+            _ => {
+                panic!("Error at index: {}", id);
+            }
+        }
+    }
+
+    disk_map.iter().enumerate().fold(0, |acc, (index, value)| {
+        if *value == -1 {
+            return acc;
+        }
+        acc + (index as i64 * value)
+    })
+}
+
+fn main() {
+    println!("Part 1: {}", part1(include_str!("data.txt")));
+    println!("Part 2: {}", part2(include_str!("data.txt")));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(include_str!("test.txt")), 1928);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(include_str!("test.txt")), 2858);
+    }
+}
